d7698bcef6eb89550a28f9e9229c4c083321dfdc
Chris Scrimgeour <chris.scrimgeour@safe.com>
2023-10-10 12:05:19 -0700

    Focus follows mouse

--- init.lua
+++ init.lua
@@ -234,6 +234,7 @@ vim.o.scrolloff = 10
 
 -- Enable mouse mode
 vim.o.mouse = 'a'
+vim.o.mousefocus = true
 
 -- Sync clipboard between OS and Neovim.
 --  Remove this option if you want your OS clipboard to remain independent.
